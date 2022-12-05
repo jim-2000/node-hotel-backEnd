fe45991d5aef3d027725b411928343e775517d90
jim-2000 <jimthecaptaionofsill@example.com>
2022-12-05 16:30:31 +0600

    wellcome page

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express"
+import path from 'path'
 import dotenv from "dotenv"
 import DbConnect from "./db/DbConnect.js";
 import cors from 'cors'
@@ -11,6 +12,7 @@ dotenv.config();
 const port = 4000 || process.env.PORT
 
 // config
+app.use(express.static('public'))
 app.use(cookieParser())
 app.use(cors())
 app.use(morgan("dev"))
@@ -19,6 +21,9 @@ app.use(express.urlencoded({ limit:"30mb", extended:true}));
 
 // middlewares
 app.use("/api",AppRoute)
+app.get('/', (req, res)=>{
+    res.sendFile(path.resolve('public/wellcome.html'));
+}) // well come html page
  
 
 // app initialization
